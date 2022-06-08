2d01f81039f9c1c6ca6e9fe01d827aa1a87e3d42
木荣 <372638156@qq.com>
2022-06-08 22:12:17 +0800

    feat: useAwaitPromise / usePromise

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # v-promise
 
 [![NPM version](https://img.shields.io/npm/v/v-promise?color=a1b858&label=)](https://www.npmjs.com/package/v-promise)
+
+
+## 💗 Thanks
+
+- [aiven715/promise-hook](https://github.com/aiven715/promise-hook)
+- [scopsy/await-to-js](https://github.com/scopsy/await-to-js)
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,58 @@
-export const one = 1
-export const two = 2
+import type { Ref } from 'vue-demi'
+import { ref } from 'vue-demi'
+
+export interface AwaitPromiseReturn<T, U = Error> {
+  data: Ref<T>
+  loading: Ref<boolean>
+  promise: () => Promise<T>
+  error: Ref<U>
+}
+
+export interface Options {
+  immediate?: boolean
+}
+
+export function usePromise<T, U = Error>(p: (...args: any[]) => Promise<T>, options?: Options, ...args: any[]): AwaitPromiseReturn<T, U> {
+  const loading = ref(false)
+  const data = ref()
+  const error = ref()
+  function promise() {
+    // eslint-disable-next-line prefer-rest-params
+    const params = arguments.length > 0 ? [...arguments] : [...args]
+    loading.value = true
+    return p(...params).then<T>((res: T) => {
+      data.value = res
+      return res
+    }).catch((err) => {
+      error.value = err
+      return err
+    }).finally(() => {
+      loading.value = false
+    })
+  }
+
+  if (options?.immediate)
+    promise()
+
+  return {
+    loading,
+    data,
+    promise,
+    error,
+  }
+}
+
+export function useAwaitPromise<T, U = Error>(p: (...args: any[]) => Promise<T>, options?: Options, ...args: any[]): Promise<AwaitPromiseReturn<T, U>> {
+  const { promise, loading, data, error } = usePromise(p, {
+    ...options,
+    immediate: false,
+  }, args)
+  return promise().then<AwaitPromiseReturn<T, U>>((): any => {
+    return {
+      promise,
+      loading,
+      data,
+      error,
+    }
+  })
+}
