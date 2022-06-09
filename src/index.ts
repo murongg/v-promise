00de20155f096579fc0ba0d03fdb690127a1492c
木荣 <372638156@qq.com>
2022-06-09 14:59:28 +0800

    refactro: type split

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@ import { ref } from 'vue-demi'
 export interface PromiseReturn<T, U = Error> {
   data: Ref<T>
   loading: Ref<boolean>
-  promise: () => Promise<T>
+  promise: (...args: any[]) => Promise<T>
   error: Ref<U>
 }
 
 export interface Options {
   immediate?: boolean
 }
+
+export type PromiseFunction<T> = (...args: any[]) => Promise<T>
+export type UsePromise<R = false> = <T, U = Error>(p: PromiseFunction<T>, options?: Options, ...args: any[]) => UsePromiseReturn<T, U, R>
+export type UsePromiseReturn<T, U = Error, R = false> = R extends true ? Promise<PromiseReturn<T, U>> : PromiseReturn<T, U>
 
 /**
  *
@@ -19,15 +23,14 @@ export interface Options {
  * @param args pass to `p` arguments
  * @returns return `PromiseReturn`
  */
-export function usePromise<T, U = Error>(p: (...args: any[]) => Promise<T>, options?: Options, ...args: any[]): PromiseReturn<T, U> {
+
+export const usePromise: UsePromise = (p, options, ...args) => {
   const loading = ref(false)
   const data = ref()
   const error = ref()
-  function promise() {
-    // eslint-disable-next-line prefer-rest-params
-    const params = arguments.length > 0 ? [...arguments] : [...args]
+  function promise(...args: any[]) {
     loading.value = true
-    return p(...params).then<T>((res: T) => {
+    return p(...args).then((res) => {
       data.value = res
       return res
     }).catch((err) => {
@@ -39,7 +42,7 @@ export function usePromise<T, U = Error>(p: (...args: any[]) => Promise<T>, opti
   }
 
   if (options?.immediate)
-    promise()
+    promise(...args)
 
   return {
     loading,
@@ -56,12 +59,13 @@ export function usePromise<T, U = Error>(p: (...args: any[]) => Promise<T>, opti
  * @param args pass to `p` arguments
  * @returns return promise with `PromiseReturn`
  */
-export function useAwaitPromise<T, U = Error>(p: (...args: any[]) => Promise<T>, options?: Options, ...args: any[]): Promise<PromiseReturn<T, U>> {
+
+export const useAwaitPromise: UsePromise<true> = (p, options, ...args) => {
   const { promise, loading, data, error } = usePromise(p, {
     ...options,
     immediate: false,
   }, args)
-  return promise().then<PromiseReturn<T, U>>((): any => {
+  return promise(...args).then((): any => {
     return {
       promise,
       loading,
